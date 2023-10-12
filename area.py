badfba6221de10565367c221c0b44a111ef62f73
GlennSoria <soria.glenn1995com>
2023-10-12 19:15:48 +0800

    First Lab Quiz

---
+++ area.py
@@ -0,0 +1,5 @@
+width = 5.5
+height = 2
+
+area = width * height
+print("The Area is:", area)
---
+++ kilometers.py
@@ -0,0 +1,5 @@
+miles = 100
+
+kilometers = miles * 1.609
+
+print("The value of kilometers is:", kilometers)
